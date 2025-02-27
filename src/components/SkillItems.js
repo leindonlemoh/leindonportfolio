e1708953107784b01a85be149e18caca6a72a6b2
leindon reimoh dilan <leindonlemoh@gmail.com>
2025-02-27 21:45:27 +0800

    add supabase

--- src/components/SkillItems.js
+++ src/components/SkillItems.js
@@ -3,32 +3,49 @@ import Css from "./images/css.png";
 import Sass from "./images/sass.png";
 import BootStrap from "./images/bootstrap.png";
 import Mui from "./images/mui.png";
-import Tailwind from "./images/tailwind-css.png"
+import Tailwind from "./images/tailwind-css.png";
 import JavaScript from "./images/js.png";
-import TypeScript from "./images/typescript.png"
+import TypeScript from "./images/typescript.png";
 import ReactJs from "./images/react.png";
-import NextJs from "./images/nextjs.png"
+import NextJs from "./images/nextjs.png";
 import NodeJs from "./images/nodejs.png";
 import ExpressJs from "./images/Expressjs.png";
 import MySQL from "./images/wb.png";
-import MongoDB from "./images/mongodb.png"
-import GitLab from "./images/gitlabss.png"
+import MongoDB from "./images/mongodb.png";
+import GitLab from "./images/gitlabss.png";
 import Github from "./images/github.png";
-import WinSCP from "./images/winscp.png"
+import WinSCP from "./images/winscp.png";
 import Sourcetree from "./images/sourcetree.png";
 import Postman from "./images/postman.png";
-import Vue from './images/Vue.png'
+import Vue from "./images/Vue.png";
+import Supabase from "./images/supabase.png";
 export const skills = [
   { name: "Html", image: Html },
   { name: "Css", image: Css },
   { name: "Sass", image: Sass },
-  { name: "BootStrap", image: BootStrap},
+  { name: "BootStrap", image: BootStrap },
   { name: "Mui", image: Mui },
   { name: "Tailwind", image: Tailwind },
   { name: "JavaScript", image: JavaScript },
-  { name: "TypeScript", image: TypeScript }
+  { name: "TypeScript", image: TypeScript },
 ];
-export const frameworks = [{name:"React.Js",image:ReactJs},{name:"Next.Js",image:NextJs},{name:"Vue.js",image:Vue}]
-export   const backend = [{name:"Node.Js",image:NodeJs},{name:"Express.Js",image:ExpressJs},{name:"MySQL",image:MySQL},{name:"MongoDB",image:MongoDB}]
+export const frameworks = [
+  { name: "React.Js", image: ReactJs },
+  { name: "Next.Js", image: NextJs },
+  { name: "Vue.js", image: Vue },
+];
+export const backend = [
+  { name: "Node.Js", image: NodeJs },
+  { name: "Express.Js", image: ExpressJs },
+  { name: "MySQL", image: MySQL },
+  { name: "MongoDB", image: MongoDB },
+  { name: "Supabase", image: Supabase },
+];
 
-export const tools = [{name:"GitLab",image:GitLab},{name:"GitHub",image:Github},{name:"WinSCP",image:WinSCP},{name:"Sourcetree",image:Sourcetree},{name:"Postman",image:Postman}]
+export const tools = [
+  { name: "GitLab", image: GitLab },
+  { name: "GitHub", image: Github },
+  { name: "WinSCP", image: WinSCP },
+  { name: "Sourcetree", image: Sourcetree },
+  { name: "Postman", image: Postman },
+];
